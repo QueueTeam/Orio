cab75a6eb72064953711526f171dd9cfdde90d3e
QueueTeam <yousifalizzy@gmail.com>
2021-10-31 23:10:15 +0300

    first commit

---
+++ Training/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            int ssx = 25;
+
+
+            byte b = 254;
+
+            Console.WriteLine(~(byte)b) ;
+
+            Console.ReadKey();
+        }
+    }
+}
